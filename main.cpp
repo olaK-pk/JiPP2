95c08d19c673cf0583ecacfdfd2af8dce0dfd106
olaK-pk <91941768+olaK-pk@users.noreply.github.com>
2021-11-23 09:40:22 +0100

    LAB 7

--- main.cpp
+++ main.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
-#include "lib/include/lib.h"
-#include "lib/src/lib.cpp"
-using namespace std;
+
+class Figura {
+protected:
+    string kolor, nazwa;
+public:
+    void printKolor();
+
+    void printNazwa();
+};
+
+class Prostokat : public Figura{
+protected:
+    double a,b;
+public:
+    void obwodProstokat();
+    void poleProstokat();
+};
+
+class Kolo : public Figura{
+protected:
+    double r;
+public:
+    void obwodKolo();
+    void poleKolo();
+};
+
+
+class Trapez : public Figura{
+protected:
+    double a,b,h;
+public:
+    void obwodTrapez();
+    void poleTrapez();
+};
+
+class Trojkat : public Figura{
+protected:
+    double a,h;
+public:
+    void obwodTrojkat();
+    void poleTrojkat();
+};
+
+
+
+
 
 int main() {
-
-    Node a, b;
-
-    cout << "Node a \t";
-    a.display();
-    cout<< "Node b \t";
-    b.display();
-
-    a.updateValue(1,1);
-    b.updateValue(4,5);
-
-    cout << "Node a \t";
-    a.display();
-    cout<< "Node b \t";
-    b.display();
-
-    cout << "odleglosc to:" << pointsDistance(a,b);
-
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
